96fd9e170b52cba6669ad06243e9597dc275d4d2
Yirong Xia <summerxia@Yirongs-MacBook-Pro.local>
2020-10-11 23:21:16 -0500

    .

--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,21 @@
 function setup() {
   // put setup code here to run once
 
-    createCanvas(windowWidth, windowHeight);
+  //create canvas 500 by 500 px
+  createCanvas(windowWidth, windowHeight);
 
-    background(250,223,202);
+  background(250,223,202);
+
+  //change rectangle x,y corrdinates to the center of rectangle
+  rectMode(CENTER);
 }
 
-
 function draw() {
-  // put drawing code here to run in a loop
+  fill(127,168,185);
+  strokeWeight(10);
+  stroke(253,220,86);
+  point(mouseX, mouseY);
+  // put drawing code here
+  ellipse(mouseX, mouseY, 150, 150);
   
-  if (mousePressed){
-    strokeWeight(10);
-    stroke(253,220,86);
-    fill(127,168,185);
-      ellipse(mouseX,mouseY,100,100);
 }
